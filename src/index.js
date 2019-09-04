f44f4da59c52b259394af469c2090a0b6d08a08e
ednaustin <edramirez512@gmail.com>
2019-09-04 11:05:59 -0500

    Updated classes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 // Create a React component
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = {text: 'Click Me' };
+
+  return (
+  <div>
+    <label className="label" htmlFor="name">Enter Name: </label>
+    <input id="name" type="text" />
+    <button style={{backgroundColor: 'blue', color: 'white'}}>
+    {buttonText.text}
+    </button>
+  </div>
+);
 };
 
 // Take the React component and show it on the screen
